d0de78122464d696d22c4e91f7afc2654caab101
Mansoon pati <104287462+herio133@users.noreply.github.com>
2025-06-03 21:59:53 +0530

    Update shit.py

--- shit.py
+++ shit.py
@@ -1,12 +1,9 @@
 ''' import requests
 from bs4 import BeautifulSoup
-
 url = "https://quotes.toscrape.com/"
 response = requests.get(url)
 soup = BeautifulSoup(response.text, "html.parser")
-
 quotes = soup.find_all("div", class_="quote")
-
 for quote in quotes:
     text = quote.find("span", class_="text").get_text()
     author = quote.find("small", class_="author").get_text()
@@ -14,12 +11,10 @@ for quote in quotes:
 
 '''import requests
 from bs4 import BeautifulSoup
-
 url = "https://www.goodreads.com/list/show/1.Best_Books_Ever"
 headers = {"User-Agent": "Mozilla/5.0"}
 response = requests.get(url, headers=headers)
 soup = BeautifulSoup(response.text, "html.parser")
-
 for book in soup.select("tr[itemtype='http://schema.org/Book']")[:10]:
     title = book.select_one("a.bookTitle span").get_text(strip=True)
     author = book.select_one("a.authorName span").get_text(strip=True)
